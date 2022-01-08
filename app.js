0f6ebf92128a62bfa2b5888f58a98a629919dcc7
task11 <task11@naver.com>
2022-01-08 20:29:22 +0900

    [Chore] Set env

---
+++ app.js
@@ -0,0 +1,12 @@
+const h1 = document.querySelector("div.a:first-child h1");
+const title = document.querySelector("title");
+
+
+title.innerHTML = "zz"
+
+function handleH1() {
+  console.log("click");
+}
+
+
+h1.addEventListener("click", handleH1);
