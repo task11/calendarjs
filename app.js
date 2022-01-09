246c91b8c7b59387dca104101e736f2b86854c03
task11 <task11@naver.com>
2022-01-09 23:24:57 +0900

    [Chore] Function Init

--- app.js
+++ app.js
@@ -1,12 +1,16 @@
-const h1 = document.querySelector("div.a:first-child h1");
-const title = document.querySelector("title");
+(function () {
+  const h1 = document.querySelector("div.a:first-child h1");
+  const title = document.querySelector("title");
 
 
-title.innerHTML = "zz"
+  title.innerHTML = "zz"
 
-function handleH1() {
-  console.log("click");
-}
+  function handleH1() {
+    console.log("click");
+  }
 
 
-h1.addEventListener("click", handleH1);
+  h1.addEventListener("click", handleH1);
+})();
+
+
