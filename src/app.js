7adb1ba405361bd5705d5db04df0213701ebcac4
task11 <task11@naver.com>
2022-03-02 20:10:06 +0900

    Feat: Calendar prev/today/next button enable

--- src/app.js
+++ src/app.js
@@ -10,32 +10,53 @@ const week = {
   6: "sat"
 }
 
-function rendering() {
-  const prevMonth = new Date(dayObject.year, dayObject.month - 1, 0);
+let prevCount = -1;
+let nextCount = 0;
+
+
+function makeHTML() {
+  const prevMonth = new Date(dayObject.year, dayObject.month + prevCount, 0);
+  const currMonth = new Date(dayObject.year, dayObject.month + nextCount, 0);
+
   const prevDate = prevMonth.getDate();
   const prevDay = prevMonth.getDay();
 
-  const currMonth = new Date(dayObject.year, dayObject.month, 0);
   const currDate = currMonth.getDate();
-  const currDay = currMonth.getDay(); // 4(목)
 
 
   let template = [];
   let viewDate = 0; // 일요일부터 시작
 
-  for (let i = prevDate - prevDay; i < prevDate + 1; i++) {
-    template.push(`<div class="date prev disable ${week[viewDate++]}"> ${i} </div>`);
-    viewDateInit();
-  }
+  template.push(`
+  <div class="calendar">
+  <h1 class="title-name">${currMonth.getFullYear()}년 ${currMonth.getMonth() + 1}월</h1>
+  <div class="calendar-month">`);
 
+
+  for (let i = prevDate - prevDay; i < prevDate + 1; i++) {
+    viewDateInit();
+    template.push(`<div class="date prev disable ${week[viewDate++]}"> ${i}일</div>`);
+
+  }
 
   for (let i = 1; i < currDate + 1; i++) {
-    template.push(`<div class="date current ${week[viewDate++]}"> ${i} </div>`);
     viewDateInit();
+    if (i === 1) {
+      template.push(`<div class="date current ${week[viewDate++]}"> ${currMonth.getMonth() + 1}월 ${i}일</div>`);
+      continue;
+    }
+    template.push(`<div class="date current ${week[viewDate++]}"> ${i}일</div>`);
+
   }
 
-  for (let i = 1; viewDate < 7; i++) {
-    template.push(`<div class="date next disable ${week[viewDate++]}"> ${i} </div>`);
+  if (viewDate) {
+    for (let i = 1; viewDate < 7; i++) {
+      if (i === 1) {
+        template.push(`<div class="date current ${week[viewDate++]}"> ${currMonth.getMonth() + 2}월 ${i}일</div>`);
+        continue;
+      }
+      template.push(`<div class="date next disable ${week[viewDate++]}"> ${i}일</div>`);
+    }
   }
 
   function viewDateInit() {
@@ -44,9 +65,45 @@ function rendering() {
     }
   }
 
+  template.push(`</div>`);
+
   return template;
 }
 
-const render = rendering();
-const app = document.getElementById('root');
-app.innerHTML = render.join(' ');
+function onClickPrev() {
+  prevCount--;
+  nextCount--;
+  rendering();
+
+}
+
+function onClickNext() {
+  prevCount++;
+  nextCount++;
+  rendering();
+}
+
+function onClickToday() {
+  prevCount = -1;
+  nextCount = 0;
+  rendering();
+}
+
+function rendering() {
+  const render = makeHTML();
+  const app = document.getElementById('root');
+  app.innerHTML = render.join(' ');
+}
+
+
+rendering();
+
+
+
+const prev = document.getElementById('prev');
+const next = document.getElementById('next');
+const currDay = document.getElementById('today');
+
+prev.addEventListener('click', onClickPrev);
+next.addEventListener('click', onClickNext);
+currDay.addEventListener('click', onClickToday);
